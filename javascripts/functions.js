2a09816a623bb05492b9574d983f23b15ee0dc41
mprathyu <32463259+mprathyu@users.noreply.github.com>
2017-10-10 18:42:41 -0500

    new changes

---
+++ javascripts/functions.js
@@ -0,0 +1,16 @@
+console.log('This would be the main JS file.');
+$(document).ready(function(){
+$.getJSON( "json/list.json", function( data ) {
+  var items = [];
+  $.each( data.Models, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val.model_name, val.two, val.model_make_id+ "</li>" ); 
+  
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "main" );
+});
+           
+})
---
+++ script.js
@@ -0,0 +1,160 @@
+var app = angular.module("learningApp", ['ui.bootstrap', 'ngRoute']);
+app.controller("myCtrl", function($scope) {
+    $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.formData = {};
+    $scope.addItem = function() {
+        $scope.errortext = "";
+        console.log($scope.formData.addMe);
+        if (!$scope.formData.addMe) {
+            return;
+        }
+        if ($scope.products.indexOf($scope.formData.addMe) == -1) {
+            $scope.products.push($scope.formData.addMe);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+    };
+    $scope.removeItem = function(x) {
+        $scope.errortext = "";
+        $scope.products.splice(x, 1);
+    };
+    $scope.services = [{
+        name: 'Web Development',
+        price: 300,
+        active: true
+    }, {
+        name: 'Design',
+        price: 400,
+        active: false
+    }, {
+        name: 'Integration',
+        price: 250,
+        active: false
+    }, {
+        name: 'Training',
+        price: 220,
+        active: false
+    }];
+
+    $scope.toggleActive = function(s) {
+        s.active = !s.active;
+    };
+    $scope.head = [{
+        head: "Name",
+        column: "name"
+    }, {
+        head: "Surname",
+        column: "surname"
+    }, {
+        head: "City",
+        column: "city"
+    }];
+    $scope.body = [{
+	        "name": "Ians",
+	        "surname": "Kueller",
+	        "city": "Zeipzig"
+	    }, {
+	        "name": "Dieter",
+	        "surname": "Lumpe",
+	        "city": "Yerlin"
+	    }, {
+	        "name": "Bans",
+	        "surname": "Mueller",
+	        "city": "Xeipzig"
+	    }, {
+	        "name": "Aieter",
+	        "surname": "Numpe",
+	        "city": "Werlin"
+	    }, {
+	        "name": "Cieter",
+	        "surname": "Oumpe",
+	        "city": "Verlin"
+	    }, {
+	        "name": "Eans",
+	        "surname": "Pueller",
+	        "city": "Ueipzig"
+	    }, {
+	        "name": "Fieter",
+	        "surname": "Qumpe",
+	        "city": "Terlin"
+	    }, {
+	        "name": "Gernd",
+	        "surname": "Ranau",
+	        "city": "Suenchen"
+    }];
+
+    $scope.sort = {
+        column: 'name',
+        descending: false
+    };
+
+    $scope.selectedCls = function(column) {
+        return column == $scope.sort.column && 'sort-' + $scope.sort.descending;
+    };
+
+    $scope.changeSorting = function(column) {
+        var sort = $scope.sort;
+        if (sort.column == column) {
+            sort.descending = !sort.descending;
+        } else {
+            sort.column = column;
+            sort.descending = false;
+        }
+    };
+    // Helper method for calculating the total price
+
+    $scope.total = function() {
+
+        var total = 0;
+
+        // Use the angular forEach helper method to
+        // loop through the services array:
+
+        angular.forEach($scope.services, function(s) {
+            if (s.active) {
+                total += s.price;
+            }
+        });
+
+        return total;
+    };
+      $scope.groups = [
+    {
+      title: "Dynamic Group Header - 1",
+      content: "Dynamic Group Body - 1",
+      open: false
+    },
+    {
+      title: "Dynamic Group Header - 2",
+      content: "Dynamic Group Body - 2",
+      open: false
+    }
+  ];
+  
+  $scope.addNew = function() {
+    $scope.groups.push({
+      title: "New One Created",
+      content: "Dynamically added new one",
+      open: false
+    });
+  };
+});
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/Main", {
+            templateUrl: "main.htm"
+        })
+        .when("/Angular-Sorting", {
+            templateUrl: "Angular-Sorting.htm"
+        })
+	    .when("/Angular-Carousel", {
+            templateUrl: "Angular-Carousel.htm"
+        })
+        .when("/To-Do-list", {
+            templateUrl: "To-Do-list.htm"
+        })
+        .when("/My-Shopping-List", {
+            templateUrl: "My-Shopping-List.htm"
+        });
+});
